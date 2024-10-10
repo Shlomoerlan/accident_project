a88bba05af360b8a58a82886c5023c45afd20a84
shlomo <shlomo.erlan@gemail.com>
2024-10-10 15:36:56 +0300

    created tests for the statistic functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,10 @@ import pytest
 from database.connect import client, db
 from repository.csv_repository import insert_data_to_mongo
 
-
 @pytest.fixture(scope="function")
 def mongodb_client():
    yield client
    client.close()
-
 
 @pytest.fixture(scope="function")
 def daily_db_test(mongodb_client):
@@ -16,18 +14,14 @@ def daily_db_test(mongodb_client):
    yield db
    mongodb_client.drop_database(db_name)
 
-
 @pytest.fixture(scope="function")
 def init_test_data(daily_db_test):
     if db['daily_collection'].count_documents({}) == 0:
        insert_data_to_mongo()
-
     for collection_name in db.list_collection_names():
        daily_db_test[collection_name].drop()
        daily_db_test[collection_name].insert_many(db[collection_name].find())
-
     yield daily_db_test
-
     for collection_name in daily_db_test.list_collection_names():
        daily_db_test[collection_name].drop()
 
--- tests/db_test.py
+++ tests/db_test.py
@@ -1,5 +1,8 @@
-def test_insert_data_to_mongo():
-    insert_data_to_mongo('data.csv')
+from database.connect import daily_collection, monthly_collection
+from repository.csv_repository import insert_data_to_mongo, insert_data_to_mongo_bulk
 
+
+def test_insert_data_to_mongo():
+    insert_data_to_mongo_bulk()
     assert daily_collection.count_documents({}) > 0
     assert monthly_collection.count_documents({}) > 0
