3be2cb7221b6fb0f9b2fea10c8b609808eaf3cc7
shlomo <shlomo.erlan@gemail.com>
2024-10-10 17:04:49 +0300

    create visual file showing the diffrent with index and without

---
+++ repository/index_repository.py
@@ -0,0 +1,64 @@
+from database.connect import db, daily_collection
+
+def create_index_to_get_total_accidents():
+    daily_collection.create_index({'AREA': 1})
+    executionStats = (daily_collection
+    .find({'AREA': '883'})
+    .hint({'AREA': 1})
+    .explain()['executionStats'])
+    executionStats_without_index = (daily_collection
+    .find({'AREA': '883'})
+    .hint({'$natural': 1})
+    .explain()['executionStats'])
+    return executionStats, executionStats_without_index
+
+def create_index_get_total_accidents_by_area_and_period():
+    daily_collection.create_index({'CRASH_DATE': 1})
+    executionStats = (daily_collection
+    .find({'CRASH_DATE': '2022-12-20'})
+    .hint({'CRASH_DATE': 1})
+    .explain()['executionStats'])
+    executionStats_without_index = (daily_collection
+    .find({'CRASH_DATE': '2022-12-20'})
+    .hint({'$natural': 1})
+    .explain()['executionStats'])
+    return executionStats, executionStats_without_index
+
+def create_index_get_accidents_by_cause():
+    daily_collection.create_index({'PRIM_CONTRIBUTORY_CAUSE': 1})
+    executionStats = (daily_collection
+    .find({'PRIM_CONTRIBUTORY_CAUSE': 'FOLLOWING TOO CLOSELY'})
+    .hint({'PRIM_CONTRIBUTORY_CAUSE': 1})
+    .explain()['executionStats'])
+    executionStats_without_index = (daily_collection
+    .find({'PRIM_CONTRIBUTORY_CAUSE': 'FOLLOWING TOO CLOSELY'})
+    .hint({'$natural': 1})
+    .explain()['executionStats'])
+    return executionStats, executionStats_without_index
+
+
+res1, res2 = create_index_get_accidents_by_cause()
+print(res1)
+print(res2)
+
+def create_index():
+    db.daily_collection.createIndex({ "AREA": 1 })
+    db.daily_collection.createIndex({ "INJURIES.FATAL_INJURIES": 1 })
+    db.daily_collection.createIndex({
+      "INJURIES.INCAPACITATING_INJURIES": 1,
+      "INJURIES.NON_INCAPACITATING_INJURIES": 1
+    })
+
+daily_collection.create_index({ 'AREA': 1 })
+
+executionStats = (daily_collection
+      .find({ 'AREA': '883' })
+      .hint({ 'AREA': 1})
+      .explain()['executionStats'])
+
+
+executionStats_without_index = (daily_collection
+      .find({ 'AREA': '883' })
+      .hint({ '$natural': 1})
+      .explain()['executionStats'])
+
